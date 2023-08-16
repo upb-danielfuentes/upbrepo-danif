ea9eaef21e7d4c3cc660cebf9d99b83e83b79c8b
Daniel Fuentes <eduwin.fuentes@upb.edu.co>
2023-08-16 16:06:55 -0500

    Sentry Exception
    Sentry Error

--- python_upb/taller_15_agosto/impresiones_repetitivas/ejercicio1.py
+++ python_upb/taller_15_agosto/impresiones_repetitivas/ejercicio1.py
@@ -1,9 +1,20 @@
 # Realice un algoritmo que permita capturar el nombre de la persona
 # y lo imprima la cantidad de veces que quiera
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://3f05934c49e0a8f851715c05a7c34b31@o4505677157695488.ingest.sentry.io/4505681394073600",
+    traces_sample_rate=1.0
+)
+
 def imprimirNombre():
-    nombre = input("Ingrese su nombre: ")
-    cantidad = int(input("Ingrese la cantidad de veces que quiere imprimir su nombre: "))
-    for i in range(cantidad):
-        print(nombre)
+    try:
+        nombre = input("Ingrese su nombre: ")
+        cantidad = int(input("Ingrese la cantidad de veces que quiere imprimir su nombre: "))
+        for i in range(cantidad):
+            print(nombre)
+    except ValueError as e:
+        sentry_sdk.capture_exception(e)
+        sentry_sdk.capture_message("El valor ingresado no es un número")
 imprimirNombre()
