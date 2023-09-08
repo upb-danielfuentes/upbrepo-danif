b3b923bec993bd331d1107c01e9896344d953268
Daniel Fuentes <eduwin.fuentes@upb.edu.co>
2023-09-07 19:06:24 -0500

    sqlito4

---
+++ python_upb/fundamentos_1/poo/sqlite3/connection.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+con = sqlite3.connect("dbempleadotest.db")
+cursor = con.cursor()
+
+# Funciones para la creación de tablas
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS tbl_Empleados (
+                    id_empleado INTEGER PRIMARY KEY AUTOINCREMENT,
+                    doc_empleado VARCHAR(11) NOT NULL,
+                    nombre_empleado VARCHAR(50) NOT NULL,
+                    apellido_empleado VARCHAR(50) NOT NULL,
+                    edad_empleado INTEGER
+               )
+""")
+
+# Funciones para la inserción de datos
+sql = ("INSERT INTO tbl_Empleados VALUES (null,?,?,?,?)")
+datos_insert = ('1018436098', 'Daniel', 'Fuentes', 33)
+cursor.execute(sql, datos_insert)
+con.commit()
+
+resultado_con = cursor.execute("SELECT * FROM tbl_Empleados")
+print(resultado_con.fetchall())
+
+print ("---------------------------------")
+res = cursor.execute("SELECT nombre_empleado FROM tbl_Empleados WHERE id_empleado = 1")
+print ("---------------------------------")
+print (res.fetchone())
+
+
